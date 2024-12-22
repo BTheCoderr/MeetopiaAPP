f48a298d8ea3966f1cceee61bbac45b7d5f98e31
BTheDream <bferrell2123@gmail.com>
2024-12-22 18:26:26 -0500

    Fix React TypeScript configuration and add global type declarations

---
+++ src/types/global.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare namespace React {
+  interface ReactNode {}
+}
+
+declare module "*.svg" {
+  const content: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+} 
