55c5d95e097a136b5ff875d20a842bce47609f6e
BTheDream <bferrell2123@gmail.com>
2024-12-22 19:12:52 -0500

    Fix module system to use CommonJS

--- server/index.ts
+++ server/index.ts
@@ -6,8 +6,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 // Get directory name in ES modules
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = path.resolve()
 
 // Load environment variables
 dotenv.config()
