ce0f5a6e749cea84eb4fe6a1381e68da1c70be8b
BTheDream <bferrell2123@gmail.com>
2024-12-22 19:14:19 -0500

    Update configuration for Render deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    webpackBuildWorker: true
+  },
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
